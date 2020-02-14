e8a5663483a7f8bf5d8aa9254a5f288b6bc8a412
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-02-14 17:36:29 -0600

    added in react-bootstrap and bootstrap. created navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import MarketingPage from './components/MarketingPage';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
