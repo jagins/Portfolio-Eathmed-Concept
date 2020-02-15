15ef00d3045fa8c04056403ffe89b37581dd4a4c
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-02-15 10:09:52 -0600

    middle section cards are in with some style

--- client/src/components/MarketingPage.js
+++ client/src/components/MarketingPage.js
@@ -6,13 +6,34 @@ function MarketingPage() {
         <div className='marketing'>
             <CompanyInfo />
             <div className='marketing-middle-section'>
-               <div className='card'>
-                   <img 
-                        src="https://tinyurl.com/ufpcnar" 
-                        alt ='Picture of a Doctor'/>
-                   <p>text</p>
-                   <p>Read More</p>
-               </div>
+                <div className='card-container'>
+                    <div className='card border-0'>
+                        <img
+                            src="https://tinyurl.com/ufpcnar"
+                            alt='Doctor' />
+                        <h3>Qualifying Medical Conditions</h3>
+                        <p>Find out if you qualify to recieve benefits of Medical Marijuana.</p>
+                        <p>Read More</p>
+                    </div>
+                    <div className='card border-0'>
+                        <img
+                            src="https://tinyurl.com/tr4sjw3"
+                            alt='Marijuana in jars' />
+                        <h3>Get Your Medical Marijuana Card</h3>
+                        <p>Learn how to get your Medical Marijuana Card.
+                            It takes time, so get started now!
+                    </p>
+                        <p>Read More</p>
+                    </div>
+                    <div className='card border-0'>
+                        <img
+                            src="https://tinyurl.com/wovebbj"
+                            alt='Handshake' />
+                        <h3>Frequently Asked Questions</h3>
+                        <p>Do you have questions? We are here to help! Click read more below for a list of answers to commonly asked questions.</p>
+                        <p>Read More</p>
+                    </div>
+                </div>
 
             </div>
         </div>
