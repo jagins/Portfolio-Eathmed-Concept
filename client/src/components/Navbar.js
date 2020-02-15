e6a012503076640e1e15276f3fb87b5fe36dfbe8
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-02-14 18:27:14 -0600

    center bar elements

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,10 +3,10 @@ import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 
 function NavbarComponent() {
     return (
-        <Navbar bg='dark' variant='dark' expand='lg'>
+        <Navbar className='justify-content-center' bg='dark' variant='dark' expand='lg'>
             <Navbar.Toggle aria-controls='responsive-navbar-nav' />
             <Navbar.Collapse id='responsive-navbar-nav'>
-                <Nav className='mr-auto'>
+                <Nav className='mx-auto'>
                     <Nav.Link href='#'>Home</Nav.Link>
                     <NavDropdown bg='dark' title='Patient Information' id='collapseable-nav-dropdown'>
                         <NavDropdown.Item href='#action/3.1'>Opioid Alternative Cannabis Program</NavDropdown.Item>
@@ -16,6 +16,13 @@ function NavbarComponent() {
                         <NavDropdown.Item href='#action/3.5'>Recreationaly Marijuana</NavDropdown.Item>
                         <NavDropdown.Item href='#action/3.6'>Frequently Asked Questions</NavDropdown.Item>
                     </NavDropdown>
+                    <NavDropdown bg='dark' title='Cannabis' id='collapseable-nav-dropdown'>
+                        <NavDropdown.Item href='#action/3.1'>Plants</NavDropdown.Item>
+                        <NavDropdown.Item href='#action/3.2'>Products</NavDropdown.Item>
+                    </NavDropdown>
+                    <Nav.Link href='#'>Reviews</Nav.Link>
+                    <Nav.Link href='#'>Store</Nav.Link>
+                    <Nav.Link href='#'>Contact Us</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
