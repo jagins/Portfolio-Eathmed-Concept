611e3109448a4b38cc58ebac0ddd1fdc069a8f36
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-02-15 14:46:05 -0600

    styling footer about and explore parts done will have to break app.css into separate files for components

--- client/src/components/MarketingFooter.js
+++ client/src/components/MarketingFooter.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import StoreInfo from './StoreInfo';
 
-function MarketingFooter()
-{
-    return(
-        <div>
-
-        </div>
+function MarketingFooter() {
+    return (
+        <footer>
+            <div className='headings'>
+                <h3>About EarthMed</h3>
+                <h3>Explore Our Site</h3>
+                <h3>Store Information</h3>
+            </div>
+            <div className='information'>
+                <div className='about'>
+                    <p>
+                        EarthMed&trade; is an Illinois Medical Marijuana Dispensary located in Addison, IL. Our main mission is to legally provide medical Marijuana to qualifying patients and to educate the community regarding the medical aspects of cannabis.
+                    </p>
+                </div>
+                <div className='links'>
+                    <div>
+                        <p>Patient Information</p>
+                        <p>About</p>
+                        <p>Directions</p>
+                    </div>
+                    <div>
+                        <p>Cannabis</p>
+                        <p>Contact Us</p>
+                    </div>
+                </div>
+                <StoreInfo />
+            </div>
+        </footer>
     )
 }
 
--- client/src/components/MarketingPage.js
+++ client/src/components/MarketingPage.js
@@ -26,6 +26,7 @@ function MarketingPage() {
                 <Help />
                 <ContactForm />
             </div>
+            <MarketingFooter />
         </div>
     );
 }
