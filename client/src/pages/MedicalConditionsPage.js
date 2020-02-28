c4c1f3df3338641c25b9f2dc0b50d61834127873
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-02-28 12:54:47 -0600

    added paragraph underneath the h1

--- client/src/pages/MedicalConditionsPage.js
+++ client/src/pages/MedicalConditionsPage.js
@@ -5,6 +5,7 @@ function MedicalConditionsPage()
     return (
         <div>
             <h1>Qualifying Medcial Conditions</h1>
+            <p>While there are hundreds of conditions that can potentially be helped by the use of medical marijuana, the state of Illinois only recognizes the following conditions as treatable using cannabis. **Please note that this list will be changing as the Illinois law evolves. If you do not see your condition listed, please check back at a later date as we will be updating the list of medical conditions that the state of Illinois deems appropriate for medical marijuana use.**</p>
         </div>
     )
 }
