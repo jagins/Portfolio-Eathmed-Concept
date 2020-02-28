975d07891fbda1de554f2f95a2def56d3578c284
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-02-28 16:03:15 -0600

    dynamically loading 2 lists works just need to style

--- client/src/pages/MedicalConditionsPage.js
+++ client/src/pages/MedicalConditionsPage.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import MedicalConditonsArray from '../utils/MecialConditionsArray';
 import MedicalConditionsCard from '../components/MedicalConditionsCard';
+import {otherConditions, spinalConditions} from '../utils/OtherMedicalConditions';
 import '../Styles/InfoPage.css';
 
 function MedicalConditionsPage()
 {
     return (
         <div className='top'>
-            <h1>Qualifying Medcial Conditions</h1>
-            <p>While there are hundreds of conditions that can potentially be helped by the use of medical marijuana, the state of Illinois only recognizes the following conditions as treatable using cannabis. **Please note that this list will be changing as the Illinois law evolves. If you do not see your condition listed, please check back at a later date as we will be updating the list of medical conditions that the state of Illinois deems appropriate for medical marijuana use.**</p>
+            <h1>Qualifying Medical Conditions</h1>
+            <p className='desc'>While there are hundreds of conditions that can potentially be helped by the use of medical marijuana, the state of Illinois only recognizes the following conditions as treatable using cannabis. **Please note that this list will be changing as the Illinois law evolves. If you do not see your condition listed, please check back at a later date as we will be updating the list of medical conditions that the state of Illinois deems appropriate for medical marijuana use.**</p>
 
             <div className='condtions-container'>
                 {MedicalConditonsArray.map((condition, index) =>
@@ -17,6 +18,26 @@ function MedicalConditionsPage()
                         title={condition.title}
                         description={condition.description} 
                     />)}
+                   <h3>Additional Medical Conditions</h3>
+                   <p>The state of Illinois has approved the use of medicinal marijuana not only for the conditions listed above but also for these following conditions:</p>
+                   <ul>
+                       {otherConditions.map((condition, index) =>
+                       {
+                           if(index === 25)
+                           {
+                               return <ul>
+                                   {spinalConditions.map(c =>
+                                    {
+                                        return <li>{c}</li>
+                                    })}
+                               </ul>
+                           }
+                           else
+                           {
+                                return <li>{condition}</li>
+                           }
+                       })}
+                   </ul>
             </div>
         </div>
     )
