04e834e1d74a12c7154e70c05d0614a0d0155c7f
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-03-07 10:37:30 -0600

    added in medical card info page and imported styles and created route in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import MarketingPage from './pages/MarketingPage';
 import OpioidProgram from './pages/OpioidProgram';
 import MedicalConditionsPage from './pages/MedicalConditionsPage';
+import MedicalCardInfo from './pages/MedicalCardInfoPage';
 import NavbarComponent from './components/Navbar';
 import MarketingFooter from './components/MarketingFooter';
 import './App.css';
@@ -16,6 +17,7 @@ function App() {
           <Route exact path='/' component={MarketingPage}/>
           <Route path='/opioid-program' component={OpioidProgram}/>
           <Route path='/medical-conditions' component={MedicalConditionsPage}/>
+          <Route path='/medical-card' component={MedicalCardInfo} />
         </Switch>
         <MarketingFooter />
       </Router>
---
+++ client/src/pages/MedicalCardInfoPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../Styles/InfoPage.css';
+
+function MedicalCardInfo()
+{
+    return (
+        <div className='top'>
+            <h1>Get Your Medical Marijuana Card</h1>
+        </div>
+    )
+}
+
+export default MedicalCardInfo;
