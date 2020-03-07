b095dd039d957028ba02cf3136a0143796b59cd2
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-03-07 11:14:55 -0600

    created an array of steps to hate repeating code

--- client/src/pages/MedicalCardInfoPage.js
+++ client/src/pages/MedicalCardInfoPage.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import stepsArray from '../utils/stepsArray';
 import '../Styles/InfoPage.css';
 
-function MedicalCardInfo()
-{
+function MedicalCardInfo() {
     return (
         <div className='top'>
             <h1>Get Your Medical Marijuana Card</h1>
+            <p className='desc'>In order to qualify for a license to use and legally purchase medical marijuana, you must fulfill the following qualifications:</p>
+
+            <div className='qualifications'>
+                <ul className='qualification-list'>
+                    <li>Be 18 years of age or older (minors need two physician recommendations as well as consent from their legal guardian),</li>
+                    <li>Be diagnosed with a qualifying medical condition – click here to see a list of qualifying medical conditions,</li>
+                    <li>Have consent from a physician,</li>
+                    <li>Be an Illinois state resident with proof of residency.</li>
+                </ul>
+            </div>
+
+            <div className='steps-container'>
+                <h3>Steps To Get Started</h3>
+                <div className='steps'>
+                    {stepsArray.map((step, index) =>
+                    {
+                        return (
+                            <div className={`Step ${index + 1}`}>
+                                <p className='step'>Step {index + 1} - {step.title}</p>
+                                <p>{step.desc}</p>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
         </div>
     )
 }
---
+++ client/src/utils/stepsArray.js
@@ -0,0 +1,43 @@
+const stepsArray = [
+    {
+        title: 'Physician Certification Form',
+        desc: 'The first step to becoming a medicinal marijuana patient in Illinois is to meet with your doctor about medical marijuana. Your physician is required to fill out the Physician Certification Form and submit it via mail it to the Illinois Department of Health. Please note that you have 90 days from the date of submission to have your appointment with your doctor',
+    },
+
+    {
+        title: 'Fill Out Patient Application',
+        desc: 'As a potential medical marijuana patient, you must fill out the patient application in it’s entirety, except the caregiver portion if you do not have a caregiver'
+    },
+
+    {
+        title: 'Pay the Application Fee',
+        desc: 'There is a $100 for 1 year, $200 for 2 year, and $250 for 3 year non-refundable application fee for all patients interested in applying for the use of medicinal marijuana. If you are a veteran or enrolled in SSDI or SSI disability programs, the application fee is reduced to $50 for 1 year, $100 for 2 year, $125 for 3 year.'
+    },
+
+    {
+        title: 'Photo',
+        desc: 'In order to complete your application, you will need to send a 2×2 inch passport photo. It is important to make sure this photo is taken with a solid colored background with nobody else in the photo with you. The easiest option is to visit your local passport photo service location and have them take your photo.'
+    },
+
+    {
+        title: 'Proof of Residency',
+        desc: 'Along with your application and a photo, you will need to provide two forms of proof that you are an Illinois resident. Acceptable forms of proof include your driver’s license, voter card, bank statements or utility bills with your name and address on them.'
+    },
+
+    {
+        title: 'Proof of Age and Identity',
+        desc: 'You must also send in a photocopy of your government issued ID, such as a driver’s license, passport, or state ID.'
+    },
+
+    {
+        title: 'Veterans',
+        desc: 'If you are a veteran, you must submit a copy of your DD214 along with the reduced $50 application fee. If you receive medical treatment from the VA, you can have them send your medical records in lieu of the Physician Certification Form.'
+    },
+
+    {
+        title: 'Caregivers',
+        desc: 'If you are caregiver, you are required to fill out the Caregiver Application Form and send in the $25 caregiver application fee.'
+    }
+]
+
+export default stepsArray;
