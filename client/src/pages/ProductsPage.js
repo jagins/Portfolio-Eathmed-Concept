e8ccee8763a5d8c94170112d17e4a5c22b7eeb4d
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-03-08 13:08:13 -0500

    fixed margins for footer i think this looks nicer gives more breathing room and fixes issue with short pages

--- client/src/pages/ProductsPage.js
+++ client/src/pages/ProductsPage.js
@@ -4,8 +4,15 @@ import '../Styles/InfoPage.css';
 function ProductsPage()
 {
     return (
-        <div>
+        <div className='products top'>
             <h1>Products</h1>
+            <p>EarthMed is dedicated to providing the highest quality cannabis to our customers. There are several different ways to take medical cannabis available.</p>
+
+            <ul className='products-list'>
+                <li>Flowers – The flowering buds from Indica, Sativa, and Hybrid Cannabis strains.</li>
+                <li>Concentrates / Extracts – Created in laboratories, there are several resins and butters available in pure compound form.</li>
+                <li>Edibles / Oral – Patients can take Cannabis orally from tablets that are created in a laboratory.</li>
+            </ul>
         </div>
     )
 }
