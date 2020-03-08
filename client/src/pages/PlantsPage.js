8e410a64c5c5b480955bd13235ab726250eb00b6
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-03-08 11:52:05 -0500

    plants page created

---
+++ client/src/pages/PlantsPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../Styles/InfoPage.css'
+
+function PlantsPage()
+{
+    return (
+        <div>
+            <h1>Plants</h1>
+        </div>
+    )
+}
+
+export default PlantsPage;
