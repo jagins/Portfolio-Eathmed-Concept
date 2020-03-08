bf783fe5ccf0b7d4761c1abfca3cd614122e363d
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-03-08 11:56:34 -0500

    added in the information about plants and styles

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import MedicalConditionsPage from './pages/MedicalConditionsPage';
 import MedicalCardInfo from './pages/MedicalCardInfoPage';
 import RecreationPage from './pages/RecreationPage';
 import FaqPage from './pages/FaqPage';
+import PlantsPage from './pages/PlantsPage'
 import NavbarComponent from './components/Navbar';
 import MarketingFooter from './components/MarketingFooter';
 import './App.css';
@@ -22,6 +23,7 @@ function App() {
           <Route path='/medical-card' component={MedicalCardInfo} />
           <Route path='/recreation' component={RecreationPage} />
           <Route path='/faq' component={FaqPage} />
+          <Route path='/plants' component={PlantsPage} />
         </Switch>
         <MarketingFooter />
       </Router>
--- client/src/pages/PlantsPage.js
+++ client/src/pages/PlantsPage.js
@@ -4,8 +4,21 @@ import '../Styles/InfoPage.css'
 function PlantsPage()
 {
     return (
-        <div>
+        <div className='top'>
             <h1>Plants</h1>
+            
+            <p>There are several different types of cannabis strains. While all medical marijuana has important medicinal and healing properties, each strain of medical marijuana is different and can be used to treat different conditions and illnesses.</p>
+            
+            <p>Medical marijuana is based on two different species of cannabis, Indica and Sativa (as well as hybrid strains that mix the two). Medical marijuana strains are created from breeding and cross-breeding these plants for their different medicinal qualities. The active ingredients present in all medical marijuana strains are THC, Cannabinol (CBN) and Cannabidiol (CBD). These compounds are present in all strains of medical cannabis and are responsible for helping to alleviate the symptoms of the illnesses of medical marijuana patients.</p>
+
+            <h3>Indica</h3>
+            <p>Generally speaking, Indica strains of medicinal marijuana will provide strong body effects which are ideal for pain relief and relaxation. Due to the high CBD:THC ratio and the sleepy effect that may accompany the use of an Indica strain, consumption of Indica cannabis is preferred for night time.  Indica strains that are higher in CBD don’t produce a psychoactive “high” and also have an anti-epileptic, anti-anxiety effect. Indica is the ideal medical marijuana strain for appetite stimulation, pain relief, muscle relaxation and as an aid for sleeplessness.</p>
+
+            <h3>Sativa</h3>
+            <p>Sativa strains of medical marijuana have a higher THC:CBD ratio which causes an uplifting and inspirational “head high.” Since the CDL levels are lower in Sativa strains, the body effects and relaxation properties are less pronounced than with Indica strains. Daytime use of Sativa is preferred as the higher THC content has been shown to have a stimulating effect. Sativa strains are good for conditions of the mind as they have been shown to treat depression, stimulate appetite, increase focus, boost energy and improve moods.</p>
+
+            <h3>Hybrids</h3>
+            <p>There are a wide range of Hybrid strains of medicinal marijuana. These plants are created by selectively “blending” both Indica and Sativa strains to maximize the beneficial properties from each strain. Most Hybrid strains of medical marijuana fall into two broad groups: Indica-Sativa (Indica dominant) or Sativa-Indica (Sativa dominant) . The goal of Hybrids is to combine characteristics of each strain to minimize some of the unwanted effects of one while adding to the desired effects of the other. For example: Indica dominant medical cannabis hybrids are effective for pain relief with the Sativa component allowing the patient to maintain their energy and activity levels. Sativa dominant strains are good for stimulating appetite, with the Indica portion helping to reduce body pain and increase relaxation. Among other conditions, Hybrid cannabis is used to treat insomnia, muscle spasms, reduce inflammation, and treat anxiety.</p>
         </div>
     )
 }
