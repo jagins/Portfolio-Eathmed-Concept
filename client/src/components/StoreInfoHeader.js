a6b36425708fa83e24ee8678209fd82a9141587c
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-03-08 14:56:39 -0500

    shop now and shopping cart to store route

--- client/src/components/StoreInfoHeader.js
+++ client/src/components/StoreInfoHeader.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { MdPlace, MdPhone } from 'react-icons/md';
 import { FaRegClock, FaShoppingCart } from 'react-icons/fa';
 import { IconContext } from 'react-icons';
@@ -18,7 +19,7 @@ function StoreInfoHeader() {
                 <div className='store-info'>
                     <p className='info'><MdPhone />(847) 607-0796</p>
                     <p className='info'><FaRegClock /> View Hours</p>
-                    <p><FaShoppingCart />Shop Now</p>
+                    <Link to='/store'><FaShoppingCart />Shop Now</Link>
                 </div>
             </div>
         </IconContext.Provider>
