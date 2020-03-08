19eb3b1d2cbb6fccbac8d7ceb6f7b0342a3dd92d
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-03-08 15:44:44 -0500

    changed spacing for hours page

--- client/src/pages/HoursPage.js
+++ client/src/pages/HoursPage.js
@@ -6,13 +6,15 @@ function HoursPage()
     return (
         <div className='top'>
             <h1>Hours of operation</h1>
-            <p>Sunday – 12pm to 5pm (Medical Only)</p>
-            <p>Monday -11am to 7pm</p>
-            <p>Tuesday – 11am to 7pm</p>
-            <p>Wednesday – 11am to 7pm</p>
-            <p>Thursday – 11am to 8pm</p>
-            <p>Friday – 11am to 8pm</p>
-            <p>Saturday – 11am to 8pm</p>
+            <div className='hours'>
+                <p>Sunday – 12pm to 5pm (Medical Only)</p>
+                <p>Monday -11am to 7pm</p>
+                <p>Tuesday – 11am to 7pm</p>
+                <p>Wednesday – 11am to 7pm</p>
+                <p>Thursday – 11am to 8pm</p>
+                <p>Friday – 11am to 8pm</p>
+                <p>Saturday – 11am to 8pm</p>
+            </div>
         </div>
     )
 }
