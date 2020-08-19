16423f39b3a35c6c151c7a4dd7ffefd0bd38439b
unknown <jagins629@gmail.com>
2020-08-19 12:02:51 -0500

    added thc and desc fields to table just need to plant them to

--- backend/data/migrations/20200816131608_create_tables.js
+++ backend/data/migrations/20200816131608_create_tables.js
@@ -13,8 +13,9 @@ exports.up = function(knex) {
         table.string('product_name');
         table.integer('price');
         table.string('product_type');
-        table.string('image')
-        table.string('description')
+        table.integer('thc_amount');
+        table.string('image');
+        table.string('description');
         table.integer('company_id')
             .references('id')
             .inTable('companies')
