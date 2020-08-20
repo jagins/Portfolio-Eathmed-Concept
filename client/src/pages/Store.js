3f7337614b9c0943893e0d82015e50a788e77ca6
unknown <jagins629@gmail.com>
2020-08-20 16:33:07 -0500

    WIP store

--- client/src/pages/Store.js
+++ client/src/pages/Store.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import logo from '../images/earthmed-site-logo-bigger.png';
+import Button from 'react-bootstrap/Button';
+import '../Styles/Store.css';
 
 function Store()
 {
     return (
-        <section>
-            Store
+        <section className='store'>
+            <div>
+                <Button>Login/Register</Button>
+            </div>
+            <img src={logo} alt='Earthmed Logo'/>
         </section>
     )
 }
