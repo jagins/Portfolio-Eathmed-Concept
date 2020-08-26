58a2056287c1a5f85b193d08040614a1c685386b
unknown <jagins629@gmail.com>
2020-08-26 17:42:49 -0500

    added comments

--- backend/products/products-router.js
+++ backend/products/products-router.js
@@ -5,6 +5,8 @@ const products = require('./products-model');
 const router = express.Router();
 
 // GET /api/products
+//Endpoint for getting all products expects a query string to filter out by product type by default since front end requries it to start
+//if strain_type is in the query as well it can more than 1 seperated by a comma
 router.get('/', (req, res) => {
     if(req.query.strain_type)
     {
@@ -25,6 +27,7 @@ router.get('/', (req, res) => {
     }
 });
 
+// GET /api/products/:id
 router.get('/:id', (req, res) => {
     products.getProductsById(req.params.id)
     .then(product => {
