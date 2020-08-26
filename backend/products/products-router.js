9035819fdff3c3418f50bbf1472fe7beac964897
unknown <jagins629@gmail.com>
2020-08-26 13:26:50 -0500

    fixed query to get more than filter

--- backend/products/products-router.js
+++ backend/products/products-router.js
@@ -2,17 +2,29 @@ const express = require('express');
 
 const products = require('./products-model');
 const { query } = require('express');
+const { contentSecurityPolicy } = require('helmet');
 
 const router = express.Router();
 
 // GET /api/products
 router.get('/', (req, res) => {
-
-    products.getAllProducts().where(req.query)
-    .then(products => res.status(200).json(products))
-    .catch(err => res.status(500).json({
-        errorMessage: 'Could not retrieve products from the db'
-    }))
+    if(req.query.strain_type)
+    {
+        let strainTypeFilter = req.query.strain_type.split(',')
+        products.getAllProducts().where('product_type', req.query.product_type).andWhere('strain_type', 'in', strainTypeFilter)
+        .then(products => res.status(200).json(products))
+        .catch(err => res.status(500).json({
+            errorMessage: 'Could not retrieve products from the db'
+        }))
+    }
+    else
+    {
+        products.getAllProducts().where('product_type', req.query.product_type)
+        .then(products => res.status(200).json(products))
+        .catch(err => res.status(500).json({
+            errorMessage: 'Could not retrieve products from the db'
+        }))
+    }
 });
 
 router.get('/:id', (req, res) => {
