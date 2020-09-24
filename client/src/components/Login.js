afbe7a6666a38ef958e67a76fcd31866b81dfca5
unknown <jagins629@gmail.com>
2020-09-24 15:56:22 -0500

    added the form for accessbility and re distyles

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 import {Button} from 'react-bootstrap';
 import Navbar from './Navbar';
@@ -6,22 +6,52 @@ import '../Styles/Login.css';
 
 function Login()
 {
+    const [state, setState] = useState({
+        email: '',
+        password: ''
+    });
+
+    const handleChange = event =>
+    {
+        setState({
+            ...state,
+            [event.target.name]: event.target.value
+        });
+    }
+
+    const handleSubmit = (event) => 
+    {
+        event.preventDefault();
+        //axios call to backend here
+    }
     return (
         <div className='login'>
             <Navbar/>
-            <div>
                 <h3>Login</h3>
                 <p>New Here? <Link to='/store/register'>Create and account</Link></p>
-                <input type='text' placeholder='Email Address'/>
-                <br/>
-                <input type='password' placeholder='Password'/>
-                <br/>
-                <div className='password-link'>
-                    <Link to='/shop/forgot-password'>Forgot your password?</Link>
-                </div>
-                <br/>
-                <Button className='login-btn'>Login</Button>
-            </div>
+                <form onSubmit={handleSubmit}>
+                    <input 
+                        type='text' 
+                        placeholder='Email Address' 
+                        name='email' 
+                        value={state.email}
+                        onChange={handleChange}
+                    />
+                    <br/>
+                    <input 
+                        type='password' 
+                        placeholder='Password'
+                        name='password'
+                        value={state.password}
+                        onChange={handleChange}
+                    />
+                    <br/>
+                    <div className='password-link'>
+                        <Link to='/shop/forgot-password'>Forgot your password?</Link>
+                    </div>
+                    <br/>
+                    <Button className='login-btn' type='submit'>Login</Button>
+                    </form>
         </div>
     );
 }
