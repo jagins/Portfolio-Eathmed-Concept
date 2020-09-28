ee362f5c3188cef13f5d143e64be08d9ede01bf5
unknown <jagins629@gmail.com>
2020-09-28 14:52:52 -0500

    form validation in with yup needs testing

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Navbar from './Navbar';
 import Button from 'react-bootstrap/Button';
-
+import * as yup from 'yup';
+import 'yup-phone';
 import '../Styles/registration.css';
 
 function Register() 
 {
+    const [state, setState] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        phoneNumber: '',
+    });
+
+    let formSchema = yup.object().shape({
+        firstName: yup.string().required(),
+        lastName: yup.string().required(),
+        email: yup.string().email().required(),
+        phoneNumber: yup.string().phone().required()
+
+    })
+
+    const handleChange = (event) => {
+        setState({
+            ...state,
+            [event.target.name]: event.target.value
+        });
+    }
+
+    const submitForm = (event) => {
+        event.preventDefault();
+    }
+
+    useEffect(() => {
+        formSchema.isValid(state)
+        .then(valid => {
+            console.log('form is valid');
+
+        })
+        .then(err => {
+            console.log(err);
+        })
+    }, [state, formSchema])
     return (
         <div className='registration-form'>
             <Navbar/>
@@ -14,13 +51,38 @@ function Register()
                 <p>Let's gather some information about you. We'll use this for contact you about your orders.</p>
             </div>
             <form>
-                <input type='text' placeholder='First Name'/>
+                <input 
+                    type='text' 
+                    placeholder='First Name' 
+                    name='firstName'
+                    value={state.firstName} 
+                    onChange={handleChange}
+                />
                 <br/>
-                <input type='text' placeholder='Last Name'/>
+                <input 
+                    type='text' 
+                    placeholder='Last Name' 
+                    name='lastName'
+                    value={state.lastName} 
+                    onChange={handleChange}
+                />
                 <br/>
-                <input type='email' placeholder='Email Address'/>
+                <input 
+                    type='email' 
+                    placeholder='Email Address' 
+                    name='email'
+                    value={state.email} 
+                    onChange={handleChange}
+                />
                 <br/>
-                <input type='tel' placeholder='Phone Number' pattern='[0-9]{3}-[0-9]{3}-[0-9]{4}'/>
+                <input 
+                    type='tel' 
+                    placeholder='Phone Number' 
+                    pattern='[0-9]{3}-[0-9]{3}-[0-9]{4}' 
+                    name='phoneNumber'
+                    value={state.phoneNumber} 
+                    onChange={handleChange}
+                />
                 <br/>
                 <Button className='next-btn' disabled>Next</Button>
             </form>
