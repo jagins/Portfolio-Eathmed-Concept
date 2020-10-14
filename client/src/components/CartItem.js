cec7702cb59f76c465ca12b855a82b53b84a3ada
unknown <jagins629@gmail.com>
2020-10-14 10:52:49 -0500

    cart list item is mocked up and styled ready to be flexed in for the design of the page

--- client/src/components/CartItem.js
+++ client/src/components/CartItem.js
@@ -1,14 +1,31 @@
 import React, {useState} from 'react';
 import {FaRegTrashAlt} from 'react-icons/fa';
+import {IconContext} from 'react-icons';
+import {FaPlusCircle, FaMinusCircle} from 'react-icons/fa';
+import Button from 'react-bootstrap/Button';
 
 function CartItem(props) {
     const [price, setPrice] = useState(props.item.price);
     return (
-        <div>
-            <img src={props.item.image} alt={props.item.product_name}/>
-            <h4>{props.item.product_name}</h4>
-            <h4>${price}</h4>
-            <FaRegTrashAlt/>
+        <div className='cart-item'>
+            <div className='cart-item-header'>
+                <img src={props.item.image} alt={props.item.product_name}/>
+                <h4 className='product-name'>{props.item.product_name}</h4>
+            </div>
+            
+            <div className='cart-item-bottom'>
+                <IconContext.Provider value={{size: '28px', color: '#28a745'}}>
+                    <div className='quanity-icons'>
+                        <Button className='btn' disabled={false}><FaPlusCircle/></Button>
+                        <span>0</span>
+                        <Button className='btn' disabled={false}><FaMinusCircle/></Button>
+                    </div>
+                </IconContext.Provider>
+                <h4>${price}</h4>
+                <IconContext.Provider value={{size: '28px', color: 'gray'}}>
+                    <FaRegTrashAlt/>
+                </IconContext.Provider>
+            </div>
         </div>
     );
 }
