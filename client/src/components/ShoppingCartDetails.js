74bb1849b90b6bbd637dc9f6d2fa66b777e7bb3a
unknown <jagins629@gmail.com>
2020-11-05 11:06:30 -0600

    mockup for total line items are in and styled needs to be dyanmic still

--- client/src/components/ShoppingCartDetails.js
+++ client/src/components/ShoppingCartDetails.js
@@ -46,28 +46,53 @@ function ShoppingCartDetails(props) {
                 </div>
             </div>
 
-            <div className='things'>
-                <h3>Things to Know</h3>
-                <div>
-                    <Icon icon={clockIcon} style={{fontSize: '4rem'}}/> 
-                    <p>Please arrive within 2 hours to secure your purchase, or we may need to release your product if other customers request it. If you can't make it in, please let us know.</p>
+            <div className='group2'>
+                <div className='things'>
+                    <h3>Things to Know</h3>
+                    <div>
+                        <Icon icon={clockIcon} style={{fontSize: '4rem'}}/> 
+                        <p>Please arrive within 2 hours to secure your purchase, or we may need to release your product if other customers request it. If you can't make it in, please let us know.</p>
+                    </div>
+                    
+                    <div>
+                        <Icon icon={idCardAlt} style={{fontSize: '3rem', color: 'green'}}/>
+                        <p>You must have a valid driver's license, state ID or passport at check-in</p>
+                    </div>
+                    
+                    <div>
+                        <Icon icon={moneyBillSolid} style={{fontSize: '4rem', color: 'green'}}/>
+                        <p>We accept debit and cash (ATM on-site) but cannot take credit cards. All discounts are applied in store.</p>
+                    </div>
+                    
+                    <div> 
+                        <Icon icon={baselineVerifiedUser} style={{fontSize: '4rem', color: 'green'}}/>
+                        <p>Illinois residents 21+ may buy up to 30 grams (a little over 1 ounce of flower), 500mg of THC-infused products (like edibles), and 5 grams of concentrate products (like vape carts or concentrates). Non-residents may purchase half of these daily cumulative amounts.</p>
+                    </div>
                 </div>
                 
-                <div>
-                    <Icon icon={idCardAlt} style={{fontSize: '3rem', color: 'green'}}/>
-                    <p>You must have a valid driver's license, state ID or passport at check-in</p>
-                </div>
-                
-                <div>
-                    <Icon icon={moneyBillSolid} style={{fontSize: '4rem', color: 'green'}}/>
-                    <p>We accept debit and cash (ATM on-site) but cannot take credit cards. All discounts are applied in store.</p>
-                </div>
-                
-                <div> 
-                    <Icon icon={baselineVerifiedUser} style={{fontSize: '4rem', color: 'green'}}/>
-                    <p>Illinois residents 21+ may buy up to 30 grams (a little over 1 ounce of flower), 500mg of THC-infused products (like edibles), and 5 grams of concentrate products (like vape carts or concentrates). Non-residents may purchase half of these daily cumulative amounts.</p>
+                <div className='line-items'>
+                    <div className='line-item'>
+                        <p>Subtotal</p>
+                        <p>29.36</p>
+                    </div>
+                    
+                    <hr/>
+                    
+                    <div className='line-item'>
+                        <p>Tax</p>
+                        <p>$10.64</p>
+                    </div>
+                    
+                    <hr/>
+                    
+                    <div className='line-item'>
+                        <h3>Total</h3>
+                        <h3>$40.00</h3>
+                    </div>
                 </div>
             </div>
+
+        
         </section>
     );
 }
