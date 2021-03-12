6286ded9288d6ed78d5d6269997ab7c64190cd8b
unknown <jagins629@gmail.com>
2021-03-12 11:08:45 -0600

    WIP: css to scss...fixed chip from being misaligned on product card and other conversions

--- client/src/components/products/ProductDetails.js
+++ client/src/components/products/ProductDetails.js
@@ -1,7 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {useParams} from 'react-router-dom';
 import axios from 'axios';
-import '../../Styles/ProductDetails.css';
 import {Button} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
 import Chip from '@material-ui/core/Chip';
